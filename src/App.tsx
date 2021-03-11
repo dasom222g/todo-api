570035c1d49b817db2816d5a561a2b750099c6db
dasom <50973005+dasom222g@users.noreply.github.com>
2021-03-11 15:41:53 +0900

    show todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,27 @@
-import React from 'react';
+import React from 'react'
 import './assets/style/pages.scss'
 import './assets/style/todo.scss'
-// import {Switch, Route} from 'react-router-dom'
-// import TodoHome from './pages/TodoHome';
-// import TodoUpdate from './pages/TodoUpdate';
-// import { TodoProvider } from './context/TodoContext';
-import Practice from './Practice';
+import { Switch, Route } from 'react-router-dom'
+import TodoHome from './pages/TodoHome'
+import TodoUpdate from './pages/TodoUpdate'
+import { TodoProvider } from './context/TodoContext'
+// import Practice from './Practice';
 
 function App() {
-
   return (
     <div className="content-wrapper">
-      <Practice />
-      {/* <div className="todo">
+      {/* <Practice /> */}
+      <div className="todo">
         <TodoProvider>
           <Switch>
-            <Route
-              path="/"
-              render={() =>
-                <TodoHome />
-              }
-              exact
-            />
-            <Route
-              path="/update/:itemId"
-              render={() =>
-                <TodoUpdate />
-              }
-            />
+            <Route path="/" render={() => <TodoHome />} exact />
+            <Route path="/update/:itemId" render={() => <TodoUpdate />} />
             <Route path="/">Not found</Route>
           </Switch>
         </TodoProvider>
-      </div> */}
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
